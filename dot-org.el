d0dff37768e681edd2779a665306c33bb5676057
Martin <martin.tang365@gmail.com>
2018-05-29 17:52:40 +0100

    Move org-mode related configuration to dot-org.el

---
+++ dot-org.el
@@ -0,0 +1,45 @@
+;;; dot-org.el --- My org-mode configuration
+;;; Commentary:
+
+;;; Code:
+(use-package org :ensure org-plus-contrib
+  :defer t
+  :bind
+  (("C-c l" . org-store-link)
+   ("C-c a" . org-agenda)
+   ("C-c c" . org-capture)
+   ("C-c b" . org-iswitchb)
+   ("C-c C-x" . org-clock-goto)
+   ("C-'" . org-cycle-agenda-files))
+  :init
+  (setq org-directory "~/gtd"
+        org-capture-templates '(("a" "Add to inbox" entry (file+headline "~/gtd/inbox.org" "INBOX")
+                                 "* TODO %?
+:PROPERTIES:
+:CREATED: %U
+:END:")
+                                ("p" "Protocol" entry (file+headline "~/gtd/inbox.org" "INBOX")
+                                 "* %^{Title}\nSource: %u, %c\n #+BEGIN_QUOTE\n%i\n#+END_QUOTE\n\n\n%?")
+                                ("L" "Protocol Link" entry (file+headline "~/gtd/inbox.org" "INBOX")
+                                 "* %? [[%:link][%:description]] \nCaptured On: %U"))
+        org-refile-targets '(("~/gtd/todo.org" :maxlevel . 3)
+                             ("~/gtd/tickler.org" :maxlevel . 2))
+        org-agenda-files '("~/gtd/inbox.org" "~/gtd/todo.org" "~/gtd/tickler.org")))
+
+(use-package evil-org :ensure t :after (evil org)
+  :config
+  (add-hook 'org-mode-hook #'evil-org-mode)
+  (add-hook 'evil-org-mode-hook #'evil-org-set-key-theme)
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+(use-package server
+  :no-require
+  :hook (server-switch . (lambda () (select-frame-set-input-focus (selected-frame))))
+  :hook (after-init . server-start))
+
+(use-package org-protocol)
+
+(provide 'dot-org)
+
+;;; dot-org ends here
--- init.el
+++ init.el
@@ -209,33 +209,9 @@
   :config
   (evil-collection-init))
 (use-package evil-magit :after (evil magit) :ensure t)
-(use-package evil-org :ensure t :after (evil org)
-  :config
-  (add-hook 'org-mode-hook #'evil-org-mode)
-  (add-hook 'evil-org-mode-hook #'evil-org-set-key-theme)
-  (require 'evil-org-agenda)
-  (evil-org-agenda-set-keys))
 
 ;; Org-Mode
-
-(use-package org :ensure org-plus-contrib
-  :defer t
-  :bind
-  (("C-c l" . org-store-link)
-   ("C-c a" . org-agenda)
-   ("C-c c" . org-capture)
-   ("C-c b" . org-iswitchb)
-   ("C-c C-x" . org-clock-goto)
-   ("C-'" . org-cycle-agenda-files))
-  :init
-  (setq org-directory "~/Documents"
-        org-capture-templates '(("t" "TODO [INBOX]" entry (file+headline "~/gtd/inbox.org" "INBOX")
-                                 "* TODO %?\n  %i\n  %a")
-                                ("T" "Tickler" entry (file+headline "~/gtd/tickler.org" "TICKLER")
-                                 "* %i%? \n %U"))
-        org-refile-targets '(("~/gtd/todo.org" :maxlevel . 3)
-                             ("~/gtd/tickler.org" :maxlevel . 2))
-        org-agenda-files '("~/gtd/inbox.org" "~/gtd/todo.org" "~/gtd/tickler.org")))
+(use-package dot-org :load-path user-emacs-directory)
 
 ;; Useful Tools
 (use-package magit :ensure t :defer t :bind (("C-x g" . magit-status)))

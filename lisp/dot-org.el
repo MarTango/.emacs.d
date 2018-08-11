4a07f3ca6ec1bd6fd4265430d3773e1cd4cf7feb
Martin <martin.tang365@gmail.com>
2018-08-11 13:28:41 +0100

    Include links and headings of level 4 in clockreport

--- lisp/dot-org.el
+++ lisp/dot-org.el
@@ -31,6 +31,7 @@
                         ("~/gtd/tickler.org" :maxlevel . 2)))
   (org-refile-use-outline-path t)
   (org-outline-path-complete-in-steps nil)
+  (org-agenda-clockreport-parameter-plist '(:link t :maxlevel 4))
   (org-agenda-files '("~/gtd/inbox.org" "~/gtd/todo.org" "~/gtd/tickler.org" "~/gtd/notes.org")))
 
 ;; Some require statements to suppress some flycheck warnings - I
